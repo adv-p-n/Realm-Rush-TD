a8030bc31cc0baee545f645c786f517e8a7e414a
adv-p-n <advaithpnair170302@gmail.com>
2023-08-08 22:25:56 +0530

    Smooth enemy movement

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
-    [SerializeField] float delay = 1f;
+    [SerializeField] [Range(0f,5f)] float moveSpeed = 1f;
+    
 
     
     void Start()
@@ -18,8 +19,19 @@ public class EnemyMovement : MonoBehaviour
     {
         foreach(Waypoint waypoint in path) 
         {
-            transform.position = waypoint.transform.position;
-            yield return new WaitForSecondsRealtime(delay);
+            Vector3 startPos=transform.position;
+            Vector3 endPos=waypoint.transform.position;
+            float movePercent=0f;
+
+            transform.LookAt(endPos);
+
+            while (movePercent<1f)
+            {
+                movePercent += Time.deltaTime * moveSpeed;
+                transform.position = Vector3.Lerp(startPos, endPos, movePercent);
+                yield return new WaitForEndOfFrame();
+            }
+            
         }
     }
     
