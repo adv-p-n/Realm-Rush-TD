b3eeb758f4fc918dea73fa0780e202284da34876
adv-p-n <advaithpnair170302@gmail.com>
2023-08-10 23:00:34 +0530

    Tower shoot closest target

--- Assets/Scripts/TargetLocator.cs
+++ Assets/Scripts/TargetLocator.cs
@@ -7,19 +7,51 @@ public class TargetLocator : MonoBehaviour
 {
     [SerializeField] Transform weapon;
     [SerializeField] Transform target;
-    void Start()
-    {
-        target = FindObjectOfType<EnemyMovement>().transform;
-        
-    }
+    [SerializeField] ParticleSystem arrow;
+    float maxRange = 15f;
+    
 
     void Update()
     {
+        FindClosestTarget();
         AimWeapon();
     }
 
-     void AimWeapon()
+     void FindClosestTarget()
     {
+        Enemy[] enemy = FindObjectsOfType<Enemy>();
+        Transform closestEnemy= null;
+        float maxDistance = Mathf.Infinity;
+        foreach(Enemy target in enemy) 
+        {
+            float distance = Vector3.Distance(transform.position, target.transform.position);
+            if(distance<maxDistance)
+            {
+                closestEnemy = target.transform;
+                maxDistance = distance;
+
+            }
+        }
+        target= closestEnemy;
+    }
+
+    void AimWeapon()
+    {
+        float distance = Vector3.Distance(transform.position, target.transform.position);
         weapon.LookAt(target);
+        if (distance < maxRange)
+        {
+            Attack(true);
+        }
+        else
+        {
+            Attack(false);
+        }
+    }
+
+     void Attack(bool isActive)
+    {
+        var emissionModule = arrow.emission;
+        emissionModule.enabled=isActive;
     }
 }
